6a3e3dad8f160d52f2d584b88125d122c7102b9b
IMO <180146096+IMOitself@users.noreply.github.com>
2025-09-09 04:03:49 +0800

    feat: faster by only doing git clone if not yet done or has a different url

--- app/src/main/assets/command.sh
+++ app/src/main/assets/command.sh
@@ -1,7 +1,27 @@
 cd storage/downloads
-rm -rf <folder_name>
-git clone <repo_url> <folder_name>
+
+CLONE_REPO=false
+
+if [ -d "<folder_name>/.git" ]; then
+    CURRENT_URL=$(git -C "<folder_name>" config --get remote.origin.url)
+
+    if [ "$CURRENT_URL" = "<repo_url>" ]; then
+        git -C "<folder_name>" pull
+    else
+        CLONE_REPO=true
+    fi
+else
+    CLONE_REPO=true
+fi
+
+if [ "$CLONE_REPO" = true ]; then
+    rm -rf "<folder_name>"
+    git clone "<repo_url>" "<folder_name>"
+fi
+
 cd <folder_name>
-ls
+
 echo <output_separator>
+
+git config --get remote.origin.url
 echo $(grep -m 1 '^#' README.md)
